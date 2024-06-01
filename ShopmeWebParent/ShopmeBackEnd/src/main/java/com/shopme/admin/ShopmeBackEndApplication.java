2018763641b3e85dbf0db4591cbeefe7b2b799bd
QuangVinh03 <19130265@st.hcmuaf.edu.vn>
2024-06-01 10:40:29 +0700

    shopme project push successfully

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -2,8 +2,10 @@ package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopme.core.entity","com.shopme.admin.user"})
 public class ShopmeBackEndApplication {
 //http://localhost:8080/ShopmeAdmin
     public static void main(String[] args) {
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/ShopmeFrontEndApplication.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/ShopmeFrontEndApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopmeFrontEndApplication {
 
+public class ShopmeFrontEndApplication {
+    //http://localhost:8088/Shopme
     public static void main(String[] args) {
         SpringApplication.run(ShopmeFrontEndApplication.class, args);
     }
